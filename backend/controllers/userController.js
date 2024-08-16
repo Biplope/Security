83357e93287307fe5a2f7e707fa09f9d2d1ef917
Biplope <maharjanbiplope16@gmail.com>
2024-08-16 12:01:21 +0545

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,99 +6,100 @@ const ResetCode = require("../model/resetCodeModel");
 const cloudinary = require("cloudinary");
 
 const createUser = async (req, res) => {
-    // Step 1: Check if data is coming or not
-    console.log(req.body);
+  // Step 1: Check if data is coming or not
+  console.log(req.body);
 
-    // Step 2: Destructure the data
-    const { firstName, lastName, email, password, confirmPassword } = req.body;
+  // Step 2: Destructure the data
+  const { firstName, lastName, email, password, confirmPassword } = req.body;
 
-    // Step 3: Validate the incoming data
-    if (!firstName || !lastName || !email || !password || !confirmPassword) {
-        return res.status(400).json({
-            success: false,
-            message: "Please enter all the fields."
-        });
+  // Step 3: Validate the incoming data
+  if (!firstName || !lastName || !email || !password || !confirmPassword) {
+    return res.status(400).json({
+      success: false,
+      message: "Please enter all the fields.",
+    });
+  }
+
+  // Email Validation: Check if the email is in a valid format
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ error: "Invalid email format" });
+  }
+
+  // Password Complexity: Require passwords to include a combination of Uppercase letters, Lowercase letters, Numbers, Special characters
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+  if (!passwordRegex.test(password)) {
+    return res.status(400).json({
+      error:
+        "Password must include a combination of: Uppercase letters, Lowercase letters, Numbers, Special characters (e.g.,!, @, #, $)",
+    });
+  }
+
+  const minPasswordLength = 8;
+  if (password.length < minPasswordLength) {
+    return res.status(400).json({
+      error: `Password length must be at least ${minPasswordLength} characters`,
+    });
+  }
+
+  // Confirm Password Validation: Check if the passwords match
+  if (password !== confirmPassword) {
+    return res.status(400).json({
+      error: "Passwords do not match.",
+    });
+  }
+
+  // Step 4: Try-catch block
+  try {
+    // Step 5: Check existing user
+    const existingUserByEmail = await Users.findOne({ email: email });
+    if (existingUserByEmail) {
+      return res.status(400).json({
+        success: false,
+        message: "User with this email already exists.",
+      });
     }
 
-    // Email Validation: Check if the email is in a valid format
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        return res.status(400).json({ error: "Invalid email format" });
-    }
+    // const existingUserByUsername = await User.findOne({ username: username });
+    // if (existingUserByUsername) {
+    //     return res.status(400).json({
+    //         success: false,
+    //         message: "Username is already taken."
+    //     });
+    // }
 
-    // Password Complexity: Require passwords to include a combination of Uppercase letters, Lowercase letters, Numbers, Special characters
-    const passwordRegex =
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
-    if (!passwordRegex.test(password)) {
-        return res.status(400).json({
-            error: "Password must include a combination of: Uppercase letters, Lowercase letters, Numbers, Special characters (e.g.,!, @, #, $)"
-        });
-    }
+    // Password encryption
+    const randomSalt = await bcrypt.genSalt(10);
+    const encryptedPassword = await bcrypt.hash(password, randomSalt);
 
-    const minPasswordLength = 8;
-    if (password.length < minPasswordLength) {
-        return res.status(400).json({
-            error: `Password length must be at least ${minPasswordLength} characters`
-        });
-    }
+    // Step 6: Create new user
+    const newUser = new Users({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: encryptedPassword,
+      confirmPassword: encryptedPassword,
+    });
 
-    // Confirm Password Validation: Check if the passwords match
-    if (password !== confirmPassword) {
-        return res.status(400).json({
-            error: "Passwords do not match."
-        });
-    }
+    // Update password history for the newly registered user
+    newUser.passwordHistory = [encryptedPassword];
+    // Trim the password history to a specific depth (e.g., last 5 passwords)
+    const passwordHistoryDepth = 5;
+    newUser.passwordHistory = newUser.passwordHistory.slice(
+      -passwordHistoryDepth
+    );
 
-    // Step 4: Try-catch block
-    try {
-        // Step 5: Check existing user
-        const existingUserByEmail = await User.findOne({ email: email });
-        if (existingUserByEmail) {
-            return res.status(400).json({
-                success: false,
-                message: "User with this email already exists."
-            });
-        }
-
-        // const existingUserByUsername = await User.findOne({ username: username });
-        // if (existingUserByUsername) {
-        //     return res.status(400).json({
-        //         success: false,
-        //         message: "Username is already taken."
-        //     });
-        // }
-
-        // Password encryption
-        const randomSalt = await bcrypt.genSalt(10);
-        const encryptedPassword = await bcrypt.hash(password, randomSalt);
-
-        // Step 6: Create new user
-        const newUser = new User({
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: encryptedPassword,
-            confirmPassword: encryptedPassword,
-
-        });
-
-        // Update password history for the newly registered user
-        newUser.passwordHistory = [encryptedPassword];
-        // Trim the password history to a specific depth (e.g., last 5 passwords)
-        const passwordHistoryDepth = 5;
-        newUser.passwordHistory = newUser.passwordHistory.slice(-passwordHistoryDepth);
-
-        // Step 7: Save user and respond
-        await newUser.save();
-        res.status(201).json({
-            success: true,
-            message: "User created successfully."
-        });
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json("Server Error");
-    }
+    // Step 7: Save user and respond
+    await newUser.save();
+    res.status(201).json({
+      success: true,
+      message: "User created successfully.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Server Error");
+  }
 };
 
 // const createUser = async (req, res) => {
@@ -179,92 +180,91 @@ const loginUser = async (req, res) => {
       });
     }
 
-// Check if account is locked
-if (user.accountLocked) {
-  const lockoutDurationMillis = Date.now() - user.lastFailedLoginAttempt;
-  const lockoutDurationSeconds = lockoutDurationMillis / 1000; // convert to seconds
+    // Check if account is locked
+    if (user.accountLocked) {
+      const lockoutDurationMillis = Date.now() - user.lastFailedLoginAttempt;
+      const lockoutDurationSeconds = lockoutDurationMillis / 1000; // convert to seconds
 
-  if (lockoutDurationSeconds >= 120) { // 2 minutes in seconds
-      // Unlock the account
-      user.accountLocked = false;
-      user.failedLoginAttempts = 0;
-      await user.save();
-  } else {
-      const timeRemainingSeconds = 120 - lockoutDurationSeconds;
-      const minutes = Math.floor(timeRemainingSeconds / 60);
-      const seconds = Math.floor(timeRemainingSeconds % 60);
+      if (lockoutDurationSeconds >= 120) {
+        // 2 minutes in seconds
+        // Unlock the account
+        user.accountLocked = false;
+        user.failedLoginAttempts = 0;
+        await user.save();
+      } else {
+        const timeRemainingSeconds = 120 - lockoutDurationSeconds;
+        const minutes = Math.floor(timeRemainingSeconds / 60);
+        const seconds = Math.floor(timeRemainingSeconds % 60);
 
-      return res.status(400).json({
+        return res.status(400).json({
           success: false,
-          message: `Account is locked. Please try again later after ${minutes} minutes and ${seconds} seconds.`
-      });
-  }
-}
-  // Check password expiry
-  const checkPasswordExpiry = (user) => {
-    const passwordExpiryDays = 90; // Set the password expiry duration in days
-    const currentDate = new Date();
-    const lastPasswordChangeDate = user.passwordChangeDate || user.createdAt;
+          message: `Account is locked. Please try again later after ${minutes} minutes and ${seconds} seconds.`,
+        });
+      }
+    }
+    // Check password expiry
+    const checkPasswordExpiry = (user) => {
+      const passwordExpiryDays = 90; // Set the password expiry duration in days
+      const currentDate = new Date();
+      const lastPasswordChangeDate = user.passwordChangeDate || user.createdAt;
 
-    const daysSinceLastChange = Math.floor(
+      const daysSinceLastChange = Math.floor(
         (currentDate - lastPasswordChangeDate) / (1000 * 60 * 60 * 24)
-    );
+      );
 
-    const daysRemaining = passwordExpiryDays - daysSinceLastChange;
+      const daysRemaining = passwordExpiryDays - daysSinceLastChange;
 
-    if (daysRemaining <= 3 && daysRemaining > 0) {
+      if (daysRemaining <= 3 && daysRemaining > 0) {
         const message = `Your password will expire in ${daysRemaining} days. Please change your password.`;
         return {
-            expired: false,
-            daysRemaining: daysRemaining,
-            message: message
-          };
-        }
-
-        return {
-            expired: daysSinceLastChange >= passwordExpiryDays,
-            daysRemaining: daysRemaining,
-            message: null
+          expired: false,
+          daysRemaining: daysRemaining,
+          message: message,
         };
+      }
+
+      return {
+        expired: daysSinceLastChange >= passwordExpiryDays,
+        daysRemaining: daysRemaining,
+        message: null,
+      };
     };
- // Compare password
- const isPasswordValid = await bcrypt.compare(password, user.password);
- if (!isPasswordValid) {
-     // Increment failed login attempts and update last failed login timestamp
-     user.failedLoginAttempts += 1;
-     user.lastFailedLoginAttempt = Date.now();
+    // Compare password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      // Increment failed login attempts and update last failed login timestamp
+      user.failedLoginAttempts += 1;
+      user.lastFailedLoginAttempt = Date.now();
 
-     // Check if the maximum allowed failed attempts is reached
-     if (user.failedLoginAttempts >= 4) {
-         // Lock the account
-         user.accountLocked = true;
-         await user.save();
-         return res.json({
-             success: false,
-             message: "Account is locked. Please try again later."
-         });
-     }
-     await user.save();
-     return res.json({
-         success: false,
-         message: "Incorrect Password."
-     });
- }
+      // Check if the maximum allowed failed attempts is reached
+      if (user.failedLoginAttempts >= 4) {
+        // Lock the account
+        user.accountLocked = true;
+        await user.save();
+        return res.json({
+          success: false,
+          message: "Account is locked. Please try again later.",
+        });
+      }
+      await user.save();
+      return res.json({
+        success: false,
+        message: "Incorrect Password.",
+      });
+    }
 
- // Reset failed login attempts and last failed login timestamp on successful login
- user.failedLoginAttempts = 0;
- user.lastFailedLoginAttempt = null;
- await user.save();
+    // Reset failed login attempts and last failed login timestamp on successful login
+    user.failedLoginAttempts = 0;
+    user.lastFailedLoginAttempt = null;
+    await user.save();
 
- // Check if the account is still locked after successful login
- if (user.accountLocked) {
-     return res.json({
-         success: false,
-         message: "Account is locked. Please try again later."
-     });
- }
-
-
+    // Check if the account is still locked after successful login
+    if (user.accountLocked) {
+      return res.json({
+        success: false,
+        message: "Account is locked. Please try again later.",
+      });
+    }
 
     // user exists:  {FirstName, LastName, Email, Password} user.password
     // Comparing password
